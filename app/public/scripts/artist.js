ea7f567d631563583e89713d8ca6ae048208b23b
yk5tc <125487092@qq.com>
2017-06-18 22:55:06 +0800

    gulp

---
+++ app/public/scripts/artist.js
@@ -0,0 +1,22 @@
+let app = new Vue(
+  {
+    computed: {
+
+    },
+    mounted: function() {
+      let vue = this;
+    },
+    activated() {
+    },
+    deactivated() {
+    },
+    data: {
+    },
+    watch: {
+    },
+    methods: {
+    },
+    components: {
+    }
+  }
+).$mount('#artist');
