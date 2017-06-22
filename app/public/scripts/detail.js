adb30fc7ed1f3b62f68d84a01d550b6389638eef
yk <1254870992@qq.com>
2017-06-22 19:17:23 +0800

    new

--- app/public/scripts/detail.js
+++ app/public/scripts/detail.js
@@ -143,6 +143,35 @@ Vue.component('discuss',
     },
   }
 );
+Vue.component('pop',
+  {
+    template: '#pop',
+    computed: {
+    },
+    mounted: function() {
+    },
+    watch: {
+    },
+    activated() {
+
+    },
+    deactivated() {
+
+    },
+    data() {
+      return {
+        positionY: 0,
+        timer: null,
+      }
+    },
+    props: ['msg'],
+    methods: {
+      close(){
+        this.$emit('close')
+      }
+    }
+  }
+);
 let app = new Vue(
   {
     computed: {
@@ -164,11 +193,36 @@ let app = new Vue(
       img_url: 'public/images',
       currentView: 'overview',
       tabs: ['概览', '艺人', '泥料', '售后保障', '评论'],
-      tab_num: 0
+      tab_num: 0,
+      showAlert: false, //显示提示组件
+      msg: null, //提示的内容,
+      open: false,
+      docked: false,
+      chosen: false,
+      input_val: 1,
+      specs: ['刻龙', '刻虎'],
+      specs_num: 0,
+      min: false,
+      collected: false,
     },
     watch: {
     },
     methods: {
+      collection(){
+        if(!this.collected){
+          this.collected = true;
+          this.showAlert = true;
+          this.msg = '收藏成功';
+        }else {
+          this.collected = false;
+          this.showAlert = true;
+          this.msg = '取消收藏成功';
+        }
+
+      },
+      close(){
+        this.showAlert = false;
+      },
       linkTo(num){
         let vue = this;
         this.tab_num = num;
@@ -210,6 +264,60 @@ let app = new Vue(
       change(index) {
 
       },
+      buy(){
+        location.href = 'buy.html';
+      },
+      add(){
+        let vue = this,
+          num = parseInt(vue.input_val.toString().replace(/[^\d]/g,''));
+        if(vue.min){
+          vue.min = false;
+        }
+        num += 1;
+        vue.input_val = num;
+      },
+      minus(){
+        let vue = this,
+          num = parseInt(vue.input_val.toString().replace(/[^\d]/g,''));
+        if(num > 0){
+          num -= 1;
+          if(num === 0){
+            vue.min = true;
+          }
+          vue.input_val = num;
+        }
+      },
+      footLinkTo(index){
+        let vue = this;
+        this.specs_num = index;
+
+      },
+      prevent(event) {
+        event.preventDefault();
+        event.stopPropagation()
+      },
+      choose() {
+        if (!this.open) {
+          this.docked = true;
+          this.open = true;
+        } else {
+          this.open = false;
+          let vue = this;
+          setTimeout(function() {
+            vue.docked = false;
+          }, 300);
+        }
+      },
+      chooseSpec(){
+        let vue = this;
+        if(!vue.chosen) {
+          vue.chosen = true;
+        }
+        else {
+          vue.chosen = false;
+        }
+      }
+
     },
     components: {
     }
--- app/public/scripts/encyclopedias.js
+++ app/public/scripts/encyclopedias.js
@@ -1,3 +1,85 @@
+Vue.component('m-swipe',
+  {
+    template: '#m_swipe',
+    computed: {
+    },
+    watch: {
+    },
+    activated() {
+
+    },
+    deactivated() {
+
+    },
+    props: {
+      swipeid: {
+        type: String,
+        default: ''
+      },
+      effect: {
+        type: String,
+        default: 'slide'
+      },
+      loop: {
+        type: Boolean,
+        default: false
+      },
+      direction: {
+        type: String,
+        default: 'horizontal'
+      },
+      pagination: {
+        type: Boolean,
+        default: true
+      },
+      autoplay: {
+        type: Number,
+        default: 5000,
+      },
+      paginationType: {
+        type: String,
+        default: 'bullets'
+      },
+      spaceBetween: {
+        type: Number,
+        default: 10,
+      }
+    },
+    data(){
+      return {
+        dom:''
+      }
+    },
+    mounted() {
+      var That = this;
+      this.dom = new Swiper('.' + That.swipeid, {
+        //循环
+        loop: That.loop,
+        //分页器
+        pagination: '.swiper-pagination',
+        //分页类型
+        paginationType: That.paginationType, //fraction,progress,bullets
+        //自动播放
+        autoplay: That.autoplay,
+        //方向
+        direction: That.direction,
+        spaceBetween: That.spaceBetween,
+        //特效
+        effect: That.effect, //slide,fade,coverflow,cube
+        autoplayDisableOnInteraction: false,
+        observer: true, //修改swiper自己或子元素时，自动初始化swiper
+        observeParents: true, //修改swiper的父元素时，自动初始化swiper
+        height : window.innerHeight,
+        lazyLoading: true,
+        paginationBulletRender: function (swiper, index, className) {
+              return '<span class="en-pagination ' + className + '"></span>';
+            }
+      })
+    },
+    components: {
+    }
+  }
+);
 let app = new Vue(
   {
     computed: {
@@ -5,9 +87,18 @@ let app = new Vue(
     mounted: function() {
       let vue = this;
       vue.getProductsType();
-      vue.load();
+      let swiper = this.$refs.swiper;
+      if (swiper.dom) {
+        this.swiper = swiper.dom;
+      }
+
+      vue.getProductsOtherType();
+      let swipert = this.$refs.swipert;
+      if (swipert.dom) {
+        this.swipert = swipert.dom;
+      }
     },
-    activated() {
+      activated() {
 
     },
     deactivated() {
@@ -22,11 +113,21 @@ let app = new Vue(
       currentView: 'overview',
       tabs: [],
       tab_num: 0,
-      list: []
+      list: [],
+      swiper: '',
+
+      tabs_t: [],
+      tab_num_t: 0,
+      swipert: '',
+      list_t: []
+
     },
     watch: {
     },
     methods: {
+      search(){
+
+      },
       getProductsType() {
         let vue = this;
         jAjax({
@@ -52,13 +153,6 @@ let app = new Vue(
         });
 
       },
-      load(){
-        var swiper = new Swiper('.swiper-container.tab-container', {
-          centeredSlides: true,
-          slidesPerView: 'auto',
-          pagination: '.swiper-pagination'
-        });
-      },
       linkTo(num){
         let vue = this;
         this.tab_num = num;
@@ -80,6 +174,63 @@ let app = new Vue(
                 tmp[j] = _data.splice(0, 12);
               }
               vue.list = tmp;
+            }else {
+              console.log('no data')
+            }
+
+          },
+          error:function(){
+            console.log('error');
+          }
+        });
+
+      },
+      getProductsOtherType() {
+        let vue = this;
+        jAjax({
+          type:'get',
+          url:apis.encyclopedias_other,
+          timeOut:5000,
+          before:function(){
+            console.log('before');
+          },
+          success:function(data){
+            if(data){
+              data = JSON.parse(data);
+              vue.tabs_t = (data);
+              vue.linkToOther(0);
+            }else {
+              console.log('no data')
+            }
+
+          },
+          error:function(){
+            console.log('error');
+          }
+        });
+
+      },
+      linkToOther(num){
+        let vue = this;
+        this.tab_num_t = num;
+        jAjax({
+          type:'get',
+          url:apis.encyclopedias_other + '?id=' + num,
+          timeOut:5000,
+          before:function(){
+            console.log('before');
+          },
+          success:function(data){
+            if(data){
+              data = JSON.parse(data);
+              console.log(data);
+              let _data = data[0].data;
+              let resLength = _data.length;
+              let tmp = [];
+              for (let i = 0, j = 0; i < resLength; i += 12, j++) {
+                tmp[j] = _data.splice(0, 12);
+              }
+              vue.list_t = tmp;
             }else {
               console.log('no data')
             }
