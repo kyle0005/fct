96cf2a0c93cb672458418efd329c4a03a7b71317
yk <1254870992@qq.com>
2017-07-06 19:28:15 +0800

    new

--- app/public/scripts/buy.js
+++ app/public/scripts/buy.js
@@ -9,14 +9,21 @@ let app = new Vue(
       let vue = this;
       vue.loadAddress();
       vue.loadCarts();
-
+      vue.coupon = config.coupon;
+      vue.loadCoupon();
+      vue.calBuyPrice(vue.couponAmount);
     },
     data: {
       show_coup: false,
       coupPri: 0.08,
       carts: [],
       address: {},
-      hasAddress: false
+      hasAddress: false,
+      couponcode: '',
+      hasCoupon: false,
+      coupon: {},
+      couponAmount: 0,
+      totalPrice: 0
     },
     watch: {
     },
@@ -24,6 +31,16 @@ let app = new Vue(
       showCoup(){
         let vue = this;
         vue.show_coup = !vue.show_coup
+      },
+      showPoint(){
+        /* 计算积分 */
+        let vue = this;
+
+      },
+      showAccountAmount (){
+        /* 计算余额 */
+        let vue = this;
+
       },
       loadAddress(){
         let vue = this;
@@ -37,6 +54,64 @@ let app = new Vue(
       loadCarts(){
         let vue = this;
         vue.carts = config.carts;
+
+      },
+      loadCoupon(){
+        let vue = this;
+        if(vue.coupon.couponCode.length == 0){
+          /* 优惠券为空 */
+          vue.hasCoupon = false;
+        }else {
+          vue.hasCoupon = true;
+          vue.couponAmount = vue.coupon.couponAmount;
+        }
+      },
+      getCoupon(){
+        let vue = this;
+        jAjax({
+          type:'post',
+          url:config.coupon_url,
+          data: {
+            'validateCoupon': config.validateCoupon,
+            'couponCode': vue.couponcode,
+          },
+          timeOut:5000,
+          before:function(){
+            console.log('before');
+          },
+          success:function(data){
+            //{message:"xxx", url:"", code:200, data:""}
+            if(data){
+              data = JSON.parse(data);
+              vue.showCoup();
+              if(parseInt(data.code) == 200){
+                vue.coupon.couponAmount = data.data;
+                vue.coupon.couponCode = vue.couponcode;
+                vue.loadCoupon();
+                vue.calBuyPrice(vue.couponAmount);
+              }else {
+                vue.msg = data.message;
+                vue.showAlert = true;
+                vue.close_auto();
+              }
+            }
+
+          },
+          error:function(){
+            console.log('error');
+          }
+        });
+      },
+      calBuyPrice(coupon){
+        let vue = this, price = 0;
+        vue.carts.forEach((item) => {
+          price += parseFloat(item.promotionPrice) * parseFloat(item.buyCount)
+        });
+        coupon = coupon || 0;
+        vue.totalPrice = price - coupon;
+        vue.totalPrice = vue.totalPrice.toFixed(2);
+      },
+      pay(){
 
       }
     },
