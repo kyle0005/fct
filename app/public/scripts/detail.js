85d057affb18f5c517d93ac1cc4838c90e51bde5
yk <1254870992@qq.com>
2017-08-23 20:04:50 +0800

    sdfs

--- app/public/scripts/detail.js
+++ app/public/scripts/detail.js
@@ -37,6 +37,73 @@ Vue.component('mVideo',
     }
   }
 );
+Vue.component('m-time',
+  {
+    template: '#m_time',
+    data(){
+      return {
+        content: '',
+      }
+    },
+    props:{
+      endTime:{
+        type: String,
+        default :''
+      },
+      endText:{
+        type : String,
+        default:''
+      },
+      callback : {
+        type : Function,
+        default :''
+      }
+    },
+    mounted () {
+      this.countdowm(this.endTime)
+    },
+    methods: {
+      countdowm(timestamp){
+        let self = this, _initTime = new Date().getTime();
+        let timer = setInterval(function(){
+          let nowTime = new Date();
+          let endTime = new Date(timestamp * 1000 + _initTime);
+          let t = endTime.getTime() - nowTime.getTime();
+          // let t = endTime.getTime();
+          if(t>0){
+            let day = Math.floor(t/86400000);
+            let hour=Math.floor((t/3600000)%24);
+            let min=Math.floor((t/60000)%60);
+            let sec=Math.floor((t/1000)%60);
+            hour = hour < 10 ? "0" + hour : hour;
+            min = min < 10 ? "0" + min : min;
+            sec = sec < 10 ? "0" + sec : sec;
+            let format = '';
+            if(day > 0){
+              format =  `${day}天${hour}小时${min}分${sec}秒`;
+            }
+            if(day <= 0 && hour > 0 ){
+              format = `${hour}小时${min}分${sec}秒`;
+            }
+            if(day <= 0 && hour <= 0){
+              format =`${min}分${sec}秒`;
+            }
+            self.content = format;
+          }else{
+            clearInterval(timer);
+            self.content = self.endText;
+            self._callback();
+          }
+        },1000);
+      },
+      _callback(){
+        if(this.callback && this.callback instanceof Function){
+          this.callback(...this);
+        }
+      }
+    }
+  }
+);
 Vue.component('overview',
   {
     template: '#overview',
@@ -62,7 +129,9 @@ Vue.component('overview',
       }
     },
     methods: {
-
+      end(){
+        console.log('end')
+      }
     },
   }
 );
