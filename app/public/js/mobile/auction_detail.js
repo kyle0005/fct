ce26639f8ccc7bd55a7ce1c470ae5a31a15aed3d
yk <1254870992@qq.com>
2017-11-28 18:22:22 +0800

    auction

--- app/public/js/mobile/auction_detail.js
+++ app/public/js/mobile/auction_detail.js
@@ -110,7 +110,7 @@ Vue.component('m-swipe',
 );
 
 let _time_html = '<span :endTime="endTime" :callback="callback" >'+
-  '<slot><span class="time-block">{{ time_content.hour }}</span>:<span class="time-block">{{ time_content.min }}</span>:<span class="time-block">{{ time_content.sec }}</span></slot>' +
+  '<slot><span class="time-block">{{ time_content.hour }}</span>小时<span class="time-block">{{ time_content.min }}</span>分<span class="time-block">{{ time_content.sec }}</span>秒</slot>' +
   '</span>';
 Vue.component('m-time',
   {
@@ -192,27 +192,13 @@ let app = new Vue(
       pro_list: [],
       loading: false,
       refreshing: false,
-      currentView: 'overview',
       showAlert: false, //显示提示组件
       msg: null, //提示的内容,
       open: false,
       docked: false,
       chosen: false,
-      input_val: 1,
-      specs_single: [],
-      specs_num: 0,
-      min: false,   /* 产品数量最小值 */
-      max: false,   /* 产品数量最大值 */
-      collected: config.product.favoriteState,
-      numsshow: false,
-      isbuy:false,
-      cart_num: config.product.cartProductCount,
-
-      subText: '加入购物车',
-
       swiper: '',
       tops: config.product.images,
-
 
     },
     mounted: function() {
@@ -221,95 +207,10 @@ let app = new Vue(
       if (swiper.dom) {
         this.swiper = swiper.dom;
       }
-      vue.loadcart();
-      vue.specs_single = vue.product.specification[0];
     },
     methods: {
-      loadcart(){
-        let vue = this;
-        let _num = vue.cart_num;
-        if(_num > 0){
-          vue.numsshow = true;
-        }else {
-          vue.numsshow = false;
-        }
-      },
       top(){
         tools.animate(document, {scrollTop: '0'}, 400,'ease-out');
-      },
-      collection(){
-        let vue = this;
-        jAjax({
-          type:'post',
-          url:config.fav_url,
-          timeOut:5000,
-          before:function(){
-          },
-          success:function(data){
-            //{message:"xxx", url:"", code:200, data:""}
-            if(data){
-              data = JSON.parse(data);
-              if(parseInt(data.code) == 200){
-                vue.collected = data.data.favoriteState;
-                if(vue.collected){
-                  vue.showAlert = true;
-                  vue.msg = data.message;
-                  vue.close_auto();
-                }else {
-                  vue.showAlert = true;
-                  vue.msg = data.message;
-                  vue.close_auto();
-                }
-              }
-            }
-
-          },
-          error:function(){
-          }
-        });
-      },
-      change(index) {},
-      add(){
-        let vue = this,
-          num = parseInt(vue.input_val.toString().replace(/[^\d]/g,''));
-        let _stock = 0;
-        _stock = (vue.product.specification.length <= 0) ? vue.product.stockCount: vue.specs_single.stockCount;
-        if(vue.min){
-          vue.min = false;
-        }
-        if(num < _stock){
-          num += 1;
-          if(num === _stock){
-            vue.max = true;
-          }
-        }
-        vue.input_val = num;
-      },
-      minus(){
-        let vue = this,
-          num = parseInt(vue.input_val.toString().replace(/[^\d]/g,''));
-        if(vue.max){
-          vue.max = false;
-        }
-        if(num > 0){
-          num -= 1;
-          if(num === 0){
-            vue.min = true;
-          }
-          vue.input_val = num;
-        }
-      },
-      footLinkTo(index){
-        let vue = this;
-        vue.specs_num = index;
-        vue.specs_single = vue.product.specification[index];
-        vue.input_val = 1;
-        vue.min = false;
-        vue.max = false;
-      },
-      prevent(event) {
-        event.preventDefault();
-        event.stopPropagation()
       },
       choose() {
         let vue = this;
@@ -324,56 +225,8 @@ let app = new Vue(
         }
 
       },
-      chooseSpec(){
+      add(){
         let vue = this;
-        if(!vue.chosen) {
-          vue.chosen = true;
-        }
-        else {
-          vue.chosen = false;
-        }
-      },
-      buy(num){
-        let vue = this;
-        if(parseInt(num) == 1){
-          //  立即购买
-          let _url = config.buy_url + '?product_id=' + vue.product.id;
-          if(vue.specs_single && vue.specs_single.id){
-            _url += '&spec_id=' + vue.specs_single.id;
-          }
-          _url += '&buy_number=' + vue.input_val;
-          location.href = _url;
-        }else {
-          //  加入购物车
-          let post_url = config.addcart_url,
-            post_data = formData.serializeForm('addcart');
-          vue.$refs.subpost.post(post_url, post_data);
-        }
-
-      },
-      linkTo(num){
-        let vue = this;
-        vue.tab_num = num;
-        switch(parseInt(num))
-        {
-          case 0:
-            vue.currentView ='overview';
-            break;
-          case 1:
-            vue.currentView ='artist';
-            break;
-          case 2:
-            vue.currentView ='pug';
-            break;
-          case 3:
-            vue.currentView ='service';
-            break;
-          case 4:
-            vue.currentView ='discuss';
-            break;
-          default:
-            vue.currentView ='overview';
-        }
 
       },
       end(){
@@ -381,8 +234,6 @@ let app = new Vue(
       },
       succhandle(data){
         let vue = this;
-        vue.cart_num = data.data.cartProductCount;
-        vue.loadcart();
         vue.msg = data.message;
         vue.showAlert = true;
         if(data.url){
