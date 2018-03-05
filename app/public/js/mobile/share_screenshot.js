a672a92bf3b46a017a7f1a942a2585448ee6dfe4
yk <1254870992@qq.com>
2018-03-05 18:01:42 +0800

    screenshot

--- app/public/js/mobile/share_screenshot.js
+++ app/public/js/mobile/share_screenshot.js
@@ -5,75 +5,74 @@ let app = new Vue(
     },
     mounted: function() {
       let vue = this;
-
+      vue.drawCanvas();
     },
     watch: {
 
     },
     data: {
-      showAlert: false, //显示提示组件
-      msg: null, //提示的内容
 
     },
     methods: {
-      convert2canvas() {
-        var cntElem = document.body;
+      drawCanvas(){
+        let vue = this;
+        var w_width = window.innerWidth || document.documentElement.clientWidth;
+        var w_height = window.innerHeight || document.documentElement.clientHeight;
+        var canvas = document.getElementById('con_screen');
+        canvas.width = w_width;
+        canvas.height = 970 / 750 * w_width;
 
-        var shareContent = cntElem;//需要截图的包裹的（原生的）DOM 对象
-        var width = shareContent.offsetWidth; //获取dom 宽度
-        var height = shareContent.offsetHeight; //获取dom 高度
-        var canvas = document.createElement("canvas"); //创建一个canvas节点
-        var scale = 2; //定义任意放大倍数 支持小数
-        canvas.width = width * scale; //定义canvas 宽度 * 缩放
-        canvas.height = height * scale; //定义canvas高度 *缩放
-        canvas.getContext("2d").scale(scale, scale); //获取context,设置scale
-        var opts = {
-          scale: scale, // 添加的scale 参数
-          canvas: canvas, //自定义 canvas
-          // logging: true, //日志开关，便于查看html2canvas的内部执行流程
-          width: width, //dom 原始宽度
-          height: height,
-          useCORS: true // 【重要】开启跨域配置
+        var ctx = canvas.getContext('2d');
+        ctx.fillStyle='#ffffff';
+        ctx.fillRect(0, 0, w_width, 970 / 750 * w_width);
+
+        var img=new Image();
+        img.src='http://localhost:9000/public/img/mobile/resource/pro01.png';
+        img.onload = function(){
+          ctx.drawImage(img,0, 0, w_width, img.height / img.width * w_width);
         };
 
-        html2canvas(shareContent, opts).then(function (canvas) {
+        var photo=new Image();
+        photo.src='http://localhost:9000/public/img/mobile/resource/emptyPhoto.png';
+        photo.onload = function(){
+          // ctx.drawImage(photo, ((w_width - 128 / 750 * w_width) / 2), (60 / 750 * w_width), (128 / 750 * w_width), (128 / 750 * w_width));
+          vue.circleImg(ctx, photo, ((w_width - 128 / 750 * w_width) / 2), (60 / 750 * w_width), (128 / 750 * w_width / 2));
+        };
 
-          var context = canvas.getContext('2d');
-          // 【重要】关闭抗锯齿
-          context.mozImageSmoothingEnabled = false;
-          context.webkitImageSmoothingEnabled = false;
-          context.msImageSmoothingEnabled = false;
-          context.imageSmoothingEnabled = false;
+        ctx.fillStyle='#333';
+        ctx.font='1.4rem 微软雅黑';
+        ctx.fillText('小凤鸣茗壶', 10, w_width + 30);
 
-          // 【重要】默认转化的格式为png,也可设置为其他格式
-          var img = Canvas2Image.convertToJPEG(canvas, canvas.width, canvas.height);
+        ctx.fillStyle='#666';
+        ctx.font='1.2rem 微软雅黑';
+        ctx.fillText('许艳春', 10, w_width + 60);
 
-          document.body.appendChild(img);
-          img.style.height = canvas.height / 2 + 'px';
-          img.style.width = canvas.width / 2 + 'px';
-          img.className += ' f-full';
+        ctx.fillStyle='#993333';
+        ctx.font='1.5rem 微软雅黑';
+        ctx.fillText('￥90000.00', 10, w_width + 90);
 
-        });
-      },
-      close(){
-        this.showAlert = false;
-      },
-      close_auto(callback, obj){
-        let vue = this;
-        setTimeout(function () {
-          vue.showAlert = false;
-          if(callback){
-            callback(obj);
-          }
 
-        }, 1500);
 
       },
-      linkto(url){
-        if(url){
-          location.href = url;
-        }
-      },
+     circleImg(ctx, img, x, y, r) {
+        //外圈画圆
+        ctx.save();
+        var d =2 * r;
+        var cx = x + r;
+        var cy = y + r;
+
+       ctx.beginPath();
+       ctx.lineWidth = 2;
+       ctx.strokeStyle = '#fff';
+       ctx.arc(cx, cy, r, 0, 2 * Math.PI);
+       ctx.stroke();
+       ctx.closePath();
+
+        ctx.arc(cx, cy, r, 0, 2 * Math.PI);
+        ctx.clip();
+        ctx.drawImage(img, x, y, d, d);
+        ctx.restore();
+      }
     },
   }
 ).$mount('#screenshot');
