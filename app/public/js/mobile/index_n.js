30129cdcffef0c584865726bf880e6b029ab3319
yk <1254870992@qq.com>
2018-04-21 17:38:56 +0800

    index new

--- app/public/js/mobile/index_n.js
+++ app/public/js/mobile/index_n.js
@@ -74,6 +74,80 @@ Vue.component('m-swipe',
     }
   }
 );
+let _time_html = '<span :endTime="endTime" :callback="callback" >'+
+  '<slot><span class="time-block">{{ time_content.hour }}</span>:<span class="time-block">{{ time_content.min }}</span>:<span class="time-block">{{ time_content.sec }}</span></slot>' +
+  '</span>';
+Vue.component('m-time',
+  {
+    template: _time_html,
+    data(){
+      return {
+        time_content: {
+          day: '00',
+          hour: '00',
+          min: '00',
+          sec: '00',
+        },
+      }
+    },
+    props:{
+      endTime:{
+        type: Number,
+        default :0
+      },
+      callback : {
+        type : Function,
+        default :''
+      }
+    },
+    mounted () {
+      this.countdowm(this.endTime)
+    },
+    methods: {
+      countdowm(timestamp){
+        let self = this, _initTime = new Date().getTime();
+        let timer = setInterval(function(){
+          let nowTime = new Date();
+          let endTime = new Date(timestamp * 1000 + _initTime);
+          let t = endTime.getTime() - nowTime.getTime();
+          if(t>0){
+            let day = Math.floor(t/86400000);
+            let hour=Math.floor((t/3600000)%24);
+            let min=Math.floor((t/60000)%60);
+            let sec=Math.floor((t/1000)%60);
+            hour = hour + day * 24;
+            hour = hour < 10 ? '0' + hour : hour;
+            min = min < 10 ? '0' + min : min;
+            sec = sec < 10 ? '0' + sec : sec;
+            if(day > 0){
+              // format =  `${day}天${hour}小时${min}分${sec}秒`;
+              self.time_content.hour = hour;
+              self.time_content.min = min;
+              self.time_content.sec = sec;
+            }
+            if(day <= 0 && hour > 0 ){
+              self.time_content.hour = hour;
+              self.time_content.min = min;
+              self.time_content.sec = sec;
+            }
+            if(day <= 0 && hour <= 0){
+              self.time_content.min = min;
+              self.time_content.sec = sec;
+            }
+          }else{
+            clearInterval(timer);
+            self._callback();
+          }
+        },1000);
+      },
+      _callback(){
+        if(this.callback && this.callback instanceof Function){
+          this.callback(...this);
+        }
+      }
+    }
+  }
+);
 let app = new Vue(
   {
     mounted: function () {
@@ -97,10 +171,17 @@ let app = new Vue(
       showTop: false,
       isADShow: config.isADShow,
 
+      index_data: config.index_n,
+
       swipe_banner: '',
       swipe_presale: '',
-      banners: config.images,
-      preSales: config.preSales
+      banners: config.index_n.adSwiper,
+      preSales: config.index_n.promotion,
+      auctions: config.index_n.auction,
+      artists: config.index_n.artist,
+      recommends: config.index_n.product,
+
+      tagN: config.index_n.tagNames
 
     },
     watch: {
@@ -110,6 +191,9 @@ let app = new Vue(
       initData(){
         let vue = this;
 
+      },
+      end(){
+        console.log('end')
       },
       top(){
         tools.animate(document, {scrollTop: '0'}, 400,'ease-out');
