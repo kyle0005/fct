4fbd9f087d6d3e695eb287877ce94e799da4c695
zikai <zikai@fangcuntan.com>
2018-06-06 17:58:30 +0800

    index_search_n

--- app/public/js/mobile/index_search_n.js
+++ app/public/js/mobile/index_search_n.js
@@ -1,309 +1,3 @@
-Vue.component('head-sorts',
-  {
-    template: '#head_sorts',
-    computed: {
-    },
-    mounted: function() {
-      let vue = this;
-      vue.getTypeList();
-    },
-    props: {
-
-    },
-    data() {
-      return {
-        open: false,
-        docked: false,
-        transitionName: 'slide-left',
-        typeList: config.search.sorts,
-      }
-    },
-    methods: {
-      change(code) {
-        let vue = this;
-        vue.$emit('changelist',code);
-      },
-      toggle() {
-        if (!this.open) {
-          this.docked = true;
-          this.open = true;
-        } else {
-          this.open = false;
-          let vue = this;
-          setTimeout(function() {
-            vue.docked = false;
-          }, 300);
-        }
-      },
-      getTypeList() {
-        let vue = this;
-        vue.typeList = config.search.sorts;
-
-      },
-    },
-    components: {
-    }
-  }
-);
-Vue.component('head-artists',
-  {
-    template: '#head_artists',
-    computed: {
-    },
-    mounted: function() {
-      let vue = this;
-      vue.getTypeList();
-    },
-    data() {
-      return {
-        open: false,
-        docked: false,
-        transitionName: 'slide-left',
-        typeList: [],
-      }
-    },
-    methods: {
-      change(code) {
-        let vue = this;
-          vue.$emit('changelist',code);
-      },
-      toggle() {
-        if (!this.open) {
-          this.docked = true;
-          this.open = true;
-        } else {
-          this.open = false;
-          let vue = this;
-          setTimeout(function() {
-            vue.docked = false;
-          }, 300);
-        }
-      },
-      getTypeList() {
-        let vue = this;
-        vue.typeList = config.search.artists;
-
-      },
-    },
-    components: {
-    }
-  }
-);
-Vue.component('head-pricesorts',
-  {
-    template: '#head_priceSorts',
-    computed: {
-    },
-    mounted: function() {
-      let vue = this;
-      vue.getTypeList();
-    },
-    props: {
-      isindex: {
-        type: Boolean,
-        default: false
-      },
-    },
-    data() {
-      return {
-        open: false,
-        docked: false,
-        transitionName: 'slide-left',
-        typeList: [],
-        loading: false,
-        refreshing: false,
-        // img_url: 'public/img/mobile',
-        pro_list_type: []
-      }
-    },
-    methods: {
-      toIndex(){
-        location.href = config.index;
-      },
-      toLogin(){
-        location.href = config.login;
-      },
-      change(code) {
-        let vue = this;
-        if(vue.isindex){
-          /* 判断为首页 */
-          vue.$emit('changelist',code);
-        }
-        else {
-          /* 不为首页 */
-          location.href = config.product_url + '?code=' + code;
-        }
-
-      },
-      toggle() {
-        if (!this.open) {
-          this.docked = true;
-          this.open = true;
-        } else {
-          this.open = false;
-          let vue = this;
-          setTimeout(function() {
-            vue.docked = false;
-          }, 300);
-        }
-      },
-      prevent(event) {
-        event.preventDefault();
-        event.stopPropagation()
-      },
-      getTypeList() {
-        let vue = this;
-        vue.typeList = config.productsType;
-
-      },
-    },
-    components: {
-    }
-  }
-);
-Vue.component('head-volumes',
-  {
-    template: '#head_volumes',
-    computed: {
-    },
-    mounted: function() {
-      let vue = this;
-      vue.getTypeList();
-    },
-    props: {
-      isindex: {
-        type: Boolean,
-        default: false
-      },
-    },
-    data() {
-      return {
-        open: false,
-        docked: false,
-        transitionName: 'slide-left',
-        typeList: [],
-        loading: false,
-        refreshing: false,
-        // img_url: 'public/img/mobile',
-        pro_list_type: []
-      }
-    },
-    methods: {
-      toIndex(){
-        location.href = config.index;
-      },
-      toLogin(){
-        location.href = config.login;
-      },
-      change(code) {
-        let vue = this;
-        if(vue.isindex){
-          /* 判断为首页 */
-          vue.$emit('changelist',code);
-        }
-        else {
-          /* 不为首页 */
-          location.href = config.product_url + '?code=' + code;
-        }
-
-      },
-      toggle() {
-        if (!this.open) {
-          this.docked = true;
-          this.open = true;
-        } else {
-          this.open = false;
-          let vue = this;
-          setTimeout(function() {
-            vue.docked = false;
-          }, 300);
-        }
-      },
-      prevent(event) {
-        event.preventDefault();
-        event.stopPropagation()
-      },
-      getTypeList() {
-        let vue = this;
-        vue.typeList = config.productsType;
-
-      },
-    },
-    components: {
-    }
-  }
-);
-Vue.component('head-top',
-  {
-    template: '#head_top',
-    computed: {
-    },
-    mounted: function() {
-      let vue = this;
-      vue.getTypeList();
-    },
-    props: {
-      isindex: {
-        type: Boolean,
-        default: false
-      },
-    },
-    data() {
-      return {
-        open: false,
-        docked: false,
-        transitionName: 'slide-left',
-        typeList: [],
-        loading: false,
-        refreshing: false,
-        pro_list_type: []
-      }
-    },
-    methods: {
-      toIndex(){
-        location.href = config.index;
-      },
-      toLogin(){
-        location.href = config.login;
-      },
-      change(code) {
-        let vue = this;
-        if(vue.isindex){
-          /* 判断为首页 */
-          vue.$emit('changelist',code);
-        }
-        else {
-          /* 不为首页 */
-          location.href = config.product_url + '?code=' + code;
-        }
-
-      },
-      toggle() {
-        if (!this.open) {
-          this.docked = true;
-          this.open = true;
-        } else {
-          this.open = false;
-          let vue = this;
-          setTimeout(function() {
-            vue.docked = false;
-          }, 300);
-        }
-      },
-      prevent(event) {
-        event.preventDefault();
-        event.stopPropagation()
-      },
-      getTypeList() {
-        let vue = this;
-        vue.typeList = config.productsType;
-
-      },
-    },
-    components: {
-    }
-  }
-);
 let app = new Vue(
   {
     mounted: function () {
@@ -313,14 +7,47 @@ let app = new Vue(
       }, false)
     },
     data: {
+      open: false,
+      docked: false,
+      showPop: 0,
+      sorts: config.search.sorts,
+      artists: config.search.artists,
+      priceSorts: config.search.priceSorts,
+      volumes: config.search.volumes,
+      categorys: config.search.categorys,
+
       showTop: false,
-      search: config.keyword,
-      isearch: config.isearch
+      search: config.search.keyword,
+      isearch: config.result,
+
+
     },
     watch: {
 
     },
     methods: {
+      toggle(num){
+        let vue = this;
+        if(num !== vue.showPop){
+          vue.open = false;
+          vue.docked = false;
+          vue.showPop = num;
+        }
+        vue.$nextTick(function () {
+          // DOM 更新后回调
+          setTimeout(function () {
+            vue.docked = !vue.docked;
+            vue.open = !vue.open;
+          },50)
+
+        });
+
+
+      },
+      change(code) {
+        let vue = this;
+
+      },
       subSearch(){
         let vue = this;
         vue.nodata = false;
